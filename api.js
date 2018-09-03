f3fea64e8e5b90c017e8e1217246675e6ff79669
Sigal Shalit Manor <sigal.shalit.manor@gmail.com>
2018-09-03 15:47:11 +0300

    adding heroku

--- api.js
+++ api.js
@@ -6,79 +6,79 @@ postsRouter.use(bodyParser.json());
 postsRouter.use(bodyParser.urlencoded({ extended: false }));
 
 postsRouter.get('/posts', function (req, res) {
-    Post.find().exec((err, posts)=> {
-        if (err) {
-          console.log(err);
+    Post.find({}).populate('comments').exec((err, data) => {
+        if (err){
+            console.log(err);
         }
-        console.log(posts);
-        res.send(posts);
+        console.log(data);
+        res.send(data);
     });
-    // Post.find({}).populate('comments').exec((err, data) => {
-    //     if (err) throw err;
-    //     console.log(data);
-    //     res.send(data);
-    // });
 })
 
+
 postsRouter.post('/posts', function (req, res) {
-    let {text} = req.body;
-    let newPost = new Post({ text: text, comments: [] });
+    let postText = req.body.text;
+    let newPost = new Post({ text: postText, comments: [] });
     newPost.save((err, data) => {
-        if (err) {
+        if (err){
             console.log(err);
         }
         console.log(data + ' has been saved to db');
-        res.send(text);
+        res.send(postText);
     });
-    
+
 });
 
 postsRouter.delete('/posts/:id', function (req, res) {
-    let {id} = req.params;
-    Post.findByIdAndRemove({ _id: id }, function (err) {
-        if (err) {
+    let postId = req.params.id;
+    Post.findByIdAndRemove({ _id: postId }, function (err) {
+        if (err){
             console.log(err);
         }
-        console.log('post id n: ' + id + ' has been removed');
+        console.log('post id n: ' + postId + ' has been removed');
         let clientMsg = 'The post has been deleted';
         res.send(clientMsg);
     });
 });
 
 postsRouter.post('/posts/:id/comments', function (req, res) {
-    let {id} = req.params;
+    let postId = req.params.id;
+    console.log(postId);
     let comment ={ text: req.body.text, user: req.body.user };
-    Post.findById(id, function (err, post) {
-        if (err) {
+    console.log(comment);
+    Post.findById(postId, function (err, post) {
+        if (err){
             console.log(err);
         }
         post.comments.push(comment);
-        Post.findByIdAndUpdate(id, post, function(err, comment){
-            if (err) {
+        post.save((err) => {;
+            if (err){
                 console.log(err);
             }
             console.log(comment.text + ' has been updated in the db');
             res.send(comment);
-        });
-        // post.save((err) => {
-        //     if (err) throw err;
-        //     console.log(comment.text + ' has been updated in the db');
-        //     res.send(comment);
-        // });    
+        });    
     });
 });
 
 postsRouter.delete('/posts/:id/:idComment', function (req, res) {
-    let {}
+    console.log(req.params)
     let postId = req.params.id;
     let commentId = req.params.idComment;
     Post.findById(postId, function (err, post) {
-        if (err) throw err;
+        if (err){
+            console.log(err);
+        }
         post.comments.id(commentId).remove((err)=> {
-            if (err) throw err;
+            if (err){
+                console.log(err);
+            }
         });
         post.save((err) => {
-            if (err) throw err;
+            if (err){
+                console.log(err);
+            }
+            console.log('comment deleted');
             console.log('comment id n: ' + commentId + ' has been removed from the post');
             let clientMsg = 'The comment has been deleted';
             res.send(clientMsg);
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
 const api = require('./api');
-const SERVER_PORT = 8080;
+// const SERVER_PORT = process.env.PORT || '8080';
 
 mongoose.connect('mongodb://localhost/spacebookDB', function() {
   console.log("DB connection established!!!");
@@ -17,15 +17,8 @@ app.use('/', api);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+ 
 
-//----checking the database by adding it a post with a comments----//
-
-// Post.find({}).exec(function(err, post){
-//   if (err) throw err;
-//   console.log(post);
-// });  
-
-
-app.listen(SERVER_PORT, () => {
-  console.log("Server started on port " + SERVER_PORT);
+app.listen(process.env.PORT || '8080', () => {
+  console.log("Server started on port ");
 });
